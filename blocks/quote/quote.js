0661c54fcabcd6837abea98777266032530ab07f
Shikhar Sharma <shikharsharma612@gmail.com>
2026-01-29 00:49:32 -0800

    added quote block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
-
+  console.log(block);
+  console.log(quoteWrapper);
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
